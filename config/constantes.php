cab72f4d2edde632f8f61cb37d762c3ccb7529a6
cesarmaya1006 <cesarmaya1006@gmail.com>
2024-06-11 11:06:28 -0500

    CM - 11 06 24 Com 1

---
+++ config/constantes.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "folder_doc_proyectos" => public_path() . '/documentos/proyectos/ ',
+    //--------------------------------------------------------------------------------
+    "folder_img_usuarios" => public_path() . '/imagenes/usuarios/ ',
+    //--------------------------------------------------------------------------------
+
+];
